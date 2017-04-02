49a44a3ff2cac4e3bd9ac0613e131e5f530d7e2e
Vincent Paris <vincentparis@Vincents-MacBook-Air.local>
2017-04-02 17:38:07 -0400

    Added Documentation for the Data Set

---
+++ FREDdata/R/CPIAUCSL.R
@@ -0,0 +1,13 @@
+#' Calculated US CPI Data Set Through Time
+#'
+#' This contains the Federal Reserve Bank of Saint Louis's estimated CPI for All Urban Consumers from Janurary 1947 until Feburary 2017
+#'
+#' @format A data frame with 842 rows and 2 variables:
+#' \describe{
+#'   \item{DATE}{Date of the calculated CPI, from Jan. 1, 1947 until Feburary 1, 2017}
+#'   \item{CPIAUCSL}{Estimated CPI with base year being the average of the price index for 1982-1984 seasonally adjusted}
+#'   ...
+#' }
+#' @source \url{https://fred.stlouisfed.org/series/CPIAUCSL}
+
+FRED.CPI <- read.csv("Data/CPIAUCSL.csv", stringsAsFactors = F)
